1b7c7493515d135df6769bc7e9c229aa5f47a978
beatrizmaias <beatrizbarbosamaia14@gmail.com>
2018-05-15 13:49:43 -0300

    Modelo de tabela

---
+++ JSoft/src/model/Senha.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @
+ */
+public class Senha {
+    
+    private int id;
+    private String senha;
+
+    public Senha(int id, String senha) {
+        this.id = id;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
