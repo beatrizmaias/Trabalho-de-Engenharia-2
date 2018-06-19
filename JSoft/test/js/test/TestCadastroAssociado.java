6bc677a59fe855741082e51c7b33adfed54941c0
laecioandrade <laecio_a.rodrigues@outlook.com>
2018-06-18 23:52:19 -0300

    Adicionando o teste de que verifica se todos os campos estão preenchidos

--- JSoft/test/js/test/TestCadastroAssociado.java
+++ JSoft/test/js/test/TestCadastroAssociado.java
@@ -46,4 +46,17 @@ public class TestCadastroAssociado {
        as.setTelefone("");
        assertFalse(asD.camposVazios(as));
    }
+   
+   @Test
+   public void testCamposPreenchido(){
+       as.setCpf("00000000000");
+       as.setEmail("laecio8andrade@gmail.com");
+       as.setEndereco("Rua Severo Eulalio");
+       as.setFaixa("Branca");
+       as.setIdade(19);
+       as.setNome("Laecio Andrade Rodrigues");
+       as.setPeso(64.3);
+       as.setTelefone("89994000000");
+       assertTrue(asD.camposVazios(as));
+   }
 }
