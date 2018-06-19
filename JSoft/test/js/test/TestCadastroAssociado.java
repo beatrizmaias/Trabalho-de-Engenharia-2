438a1c8a0033e8401ae077c6b18b3aa17de23eab
laecioandrade <laecio_a.rodrigues@outlook.com>
2018-06-18 23:59:17 -0300

    teste que verifica se o cadatro é valido

--- JSoft/test/js/test/TestCadastroAssociado.java
+++ JSoft/test/js/test/TestCadastroAssociado.java
@@ -35,6 +35,19 @@ public class TestCadastroAssociado {
    }
    
    @Test
+   public void testCadastroValido(){
+       as.setCpf("00000000000");
+       as.setEmail("laecio8andrade@gmail.com");
+       as.setEndereco("Rua Severo Eulalio");
+       as.setFaixa("Branca");
+       as.setIdade(19);
+       as.setNome("Laecio Andrade Rodrigues");
+       as.setPeso(64.3);
+       as.setTelefone("89994000000");
+       assertTrue(asD.adiciona(as));
+   }
+   
+   @Test
    public void testCamposVazios(){
        as.setCpf("");
        as.setEmail("");
